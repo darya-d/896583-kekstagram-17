aa11dd5be38307596267f2f29ae1911c04c5e4e7
Darya <darya.drazdouskaya@gmail.com>
2019-07-24 15:32:41 +0300

    rewrite functions, fix adding filter effects etc

--- js/main.js
+++ js/main.js
@@ -58,15 +58,18 @@ var generateListOfComments = function () {
  * @param {array} comments - список комментариев, оставленных другими пользователями к этой фотографии. Количество комментариев определяется на своё усмотрение. Все комментарии генерируются случайным образом.
  * @return {object} - описание фотографии, опубликованной пользователем.
  */
-var NUMBER_OF_LIKES_MIN = 15;
-var NUMBER_OF_LIKES_MAX = 200;
 var NUMBER_OF_PHOTO_USERS = 25;
+
+var Likes = {
+  NUMBER_MIN: 15,
+  NUMBER_MAX: 200
+};
 
 var getPhotoUsers = function (countPhotoUsers) {
   return createArray(countPhotoUsers).map(function (_value, index) {
     return {
       url: 'photos/' + (index + 1) + '.jpg',
-      likes: generateRandomNumber(NUMBER_OF_LIKES_MIN, NUMBER_OF_LIKES_MAX),
+      likes: generateRandomNumber(Likes.NUMBER_MIN, Likes.NUMBER_MAX),
       comments: generateListOfComments()
     };
   });
@@ -92,58 +95,39 @@ pictures.appendChild(fragment);
 
 
 // ==== 4th MODULE - EVENTS ====
-// Variables
-var SCALE_VALUE = {
-  MIN: 25,
-  MAX: 100
-};
-var SCALE_STEP = 25;
 var KEYCODE_ESC = 27;
+var SCALE_STEP = 25;
+var Scale = {
+  VALUE_MIN: 25,
+  VALUE_MAX: 100,
+};
 
+// ==== 4.1. Обработка изменения значения поля выбора файла #upload-file. При наступлении события change на этом поле, можно сразу показывать форму редактирования изображения.
+// ==== 4.4. Валидация: если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
 var imgEditForm = document.querySelector('.img-upload__overlay');
-
-var buttonPopupOpen = document.querySelector('#upload-file');
+var imgUploadFile = document.querySelector('#upload-file');
 var buttonPopupClose = document.querySelector('#upload-cancel');
-
-// var imgItem = document.querySelectorAll('.effects__item');
-var imgPreview = document.querySelector('.img-upload__preview > img');
-var imgPreviewButtons = document.querySelectorAll('.effects__radio');
-var imgPreviewSizeButtons = document.querySelector('.scale__control');
-var imgPreviewSize = document.querySelector('.scale__control--value').value;
-
-// var effects = {
-//   chrome: ['grayscale', 0, 1],
-//   sepia: ['sepia', 0, 1],
-//   marvin: ['invert', 0, 100 + '%'],
-//   phobos: ['blur', 0, 3 + '%'],
-//   heat: ['brightness', 1, 3]
-// };
-var effectNone = document.querySelector('.effects__preview--none');
-
-// var imgEffectPin = document.querySelector('.effect-level__pin');
-var imgEffectLine = document.querySelector('.img-upload__effect-level');
-// var imgEffectValue = document.querySelector('.effect-level__value');
-// var imgEffectValueDefault = SCALE_VALUE.MAX;
 
 var commentsArea = document.querySelector('.text__description');
 // var hashtagsArea = document.querySelector('.text__hashtags');
 
-
-// ==== 4.1. Обработка изменения значения поля выбора файла #upload-file. При наступлении события change на этом поле, можно сразу показывать форму редактирования изображения.
-// ==== 4.4. Валидация: если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
-
-buttonPopupOpen.addEventListener('change', function () {
-  imgEditForm.classList.remove('hidden');
-
-  document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === KEYCODE_ESC && evt.target !== commentsArea) {
-      imgEditForm.classList.add('hidden');
-    }
-  });
+// Create an event of closing edit form by using ESC
+var onImgEditFormEscPress = imgUploadFile.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KEYCODE_ESC && evt.target !== commentsArea) {
+    imgEditForm.classList.add('hidden');
+  }
 });
 
+// Close edit form by click
 buttonPopupClose.addEventListener('click', function () {
   imgEditForm.classList.add('hidden');
+  document.removeEventListener('keydown', onImgEditFormEscPress);
+});
+
+// Upload edit form
+imgUploadFile.addEventListener('change', function () {
+  imgEditForm.classList.remove('hidden');
+  onImgEditFormEscPress();
 });
 
 // ==== 4.2. Наложение эффекта на изображение
@@ -152,72 +136,73 @@ buttonPopupClose.addEventListener('click', function () {
 // 4.2.3. Интенсивность эффекта регулируется перемещением ползунка в слайдере .effect-level__pin. Уровень эффекта записывается в поле .effect-level__value. При изменении уровня интенсивности эффекта, CSS-стили элемента .img-upload__preview обновляются.
 // 4.2.4. При выборе эффекта «Оригинал» слайдер скрывается.
 // 4.2.5. При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%): слайдер, CSS-стиль изображения и значение поля должны обновляться.
+var imgPreview = document.querySelector('.img-upload__preview > img');
 
-imgPreviewButtons.forEach(function (radioButton) {
-  radioButton.addEventListener('click', function (evt) {
-    var imgPreviewTarget = evt.target.parentElement.querySelector('.effects__preview');
-    imgPreview.classList = imgPreviewTarget.classList[1];
-    // imgEffectLine.classList.remove('hidden');
-  });
+// var imgEffectPin = document.querySelector('.effect-level__pin');
+var imgEffectLine = document.querySelector('.img-upload__effect-level');
+// var imgEffectValue = document.querySelector('.effect-level__value');
+// var imgEffectValueDefault = Scale.VALUE_MAX;
 
-  effectNone.addEventListener('click', function () {
-    imgEffectLine.classList.add('hidden');
-    // imgEffectLine.classList.remove('hidden');
-  });
-});
 
-// var deleteEffectsClasses = function (nameOfEffect) {
-//   imgPreview.removeAttribute('class');
-//   imgPreview.classList.add(nameOfEffect);
-//   imgEffectLine.classList.remove('hidden');
+// var effects = {
+//   chrome: ['grayscale', 0, 1, ''],
+//   sepia: ['sepia', 0, 1, ''],
+//   marvin: ['invert', 0, 100, '%'],
+//   phobos: ['blur', 0, 3, '%'],
+//   heat: ['brightness', 1, 3]
 // };
+// var effectNone = document.querySelector('.effects__preview--none');
 
-// effectNone.addEventListener('click', function () {
-//   deleteEffectsClasses();
-//   imgEffectLine.classList.add('hidden');
-// });
+// Add filter effects by changing classes; show thу depth level line of filter effect
+var addEffect = function (className) {
+  imgPreview.removeAttribute('class');
+  imgPreview.classList.add(className);
+  imgEffectLine.classList.remove('hidden');
+};
 
+// Add classes for filter effects
+var changeEffect = function (evt) {
+  switch (true) {
+    case evt.target.classList.contains('effects__preview--none'):
+      addEffect('effects__preview--none');
+      imgEffectLine.classList.add('hidden');
+      break;
+    case evt.target.classList.contains('effects__preview--chrome'):
+      addEffect('effects__preview--chrome');
+      break;
+    case evt.target.classList.contains('effects__preview--sepia'):
+      addEffect('effects__preview--sepia');
+      break;
+    case evt.target.classList.contains('effects__preview--marvin'):
+      addEffect('effects__preview--marvin');
+      break;
+    case evt.target.classList.contains('effects__preview--phobos'):
+      addEffect('effects__preview--phobos');
+      break;
+    case evt.target.classList.contains('effects__preview--heat'):
+      addEffect('effects__preview--heat');
+      break;
+  }
+};
+// 'Click' event, which calls change of filter effects
+imgEditForm.addEventListener('click', changeEffect);
 
 // ==== 4.3. Масштаб
 // 4.3.1. При нажатии на кнопки .scale__control--smaller и .scale__control--bigger должно изменяться значение поля .scale__control--value. Значение должно изменяться с шагом в 25. Например, если значение поля установлено в 50%, после нажатия на «+», значение должно стать равным 75%. Максимальное значение — 100%, минимальное — 25%. Значение по умолчанию — 100%;
 // 4.3.2. При изменении значения поля .scale__control--value изображению внутри .img-upload__preview должен добавляться соответствующий стиль CSS, который с помощью трансформации scale задаёт масштаб. Например, если в поле стоит значение 75%, то в стиле изображения должно быть написано transform: scale(0.75).
-// var TRANSFORM_SCALE_MAX = 1;
-// var imgPreviewTransform = imgPreviewTransform.TRANSFORM_SCALE_MAX;
+var imgPreviewSizeButtons = document.querySelector('.scale__control');
+var imgPreviewSize = parseInt(document.querySelector('.scale__control--value').value, 10);
 
-// var changeTransformScale = function (valueOfScale) {
-//   imgPreview.style.transform = valueOfScale;
-// };
-// //  changeImgScale('scale(1)');
-
-// var decreaseValue = function () {
-//   if (imgPreviewSize > SCALE_VALUE.MIN) {
-//     imgPreviewSize = imgPreviewSize - SCALE_STEP;
-//   }
-// };
-
-// var increaseValue = function () {
-//   if (imgPreviewSize < SCALE_VALUE.MAX) {
-//     imgPreviewSize = imgPreviewSize + SCALE_STEP;
-//   }
-// };
-
-// document.addEventListener('click', function (evt) {
-//   if (evt.target.classList.contains('scale__control--bigger')) {
-//     increaseValue();
-//   } else if (evt.target.classList.contains('scale__control--smaller')) {
-//     decreaseValue();
-//   } return imgEffectValueDefault;
-// });
-
-var changeSizeOfImgPreview = function (button) {
-  if ((button.target.classList.contains('scale__control--bigger') && imgPreviewSize < SCALE_VALUE.MAX)) {
+var changeSizeOfImgPreview = function (evt) {
+  if ((evt.target.classList.contains('scale__control--bigger') && imgPreviewSize < Scale.VALUE_MAX)) {
     imgPreviewSize += SCALE_STEP;
-  } else if ((button.target.classList.contains('scale__control--smaller') && imgPreviewSize > SCALE_VALUE.MIN)) {
+  } else if ((evt.target.classList.contains('scale__control--smaller') && imgPreviewSize > Scale.VALUE_MIN)) {
     imgPreviewSize -= SCALE_STEP;
   }
 };
 
 imgPreviewSizeButtons.addEventListener('click', changeSizeOfImgPreview);
+
 // Предыдущая попытка
 // var changeValue = function (increase, decrease) {
 //   var value = parseInt(document.querySelector('.scale__control--value').value, 10);
