3f6ef200bcbf3556ebd616135afbf46920bd564f
Darya <darya.drazdouskaya@gmail.com>
2019-08-07 21:19:44 +0300

    add minor changes of the code for the second project protection

--- js/gallery.js
+++ js/gallery.js
@@ -5,13 +5,13 @@
   var MAX_PHOTOS = 10;
   var DEBOUNCE_INTERVAL = 500;
   var filtersForm = document.querySelector('.img-filters__form');
-  var filtersbuttonAll = document.querySelectorAll('.img-filters__button');
+  var filtersbuttonAll = Array.from(document.querySelectorAll('.img-filters__button'));
   var pictures = document.querySelector('.pictures');
 
   // Блок, с помощью которого производится фильтрация фотографий, скрыт изначально и показывается только после окончания загрузки всех фотографий.
   var clearImg = function () {
     var picturesCollection = pictures.querySelectorAll('.picture');
-    var picturesArray = picturesCollection;
+    var picturesArray = Array.from(picturesCollection);
     picturesArray.forEach(function (it) {
       pictures.removeChild(it);
     });
@@ -22,21 +22,21 @@
       case (evtChange.target.id === 'filter-popular'):
         clearImg();
         var popularPhotos = window.pictureUsers;
-        window.renderImg.addPicture(popularPhotos);
+        window.render.addPicture(popularPhotos);
         break;
       case (evtChange.target.id === 'filter-new'):
         clearImg();
         var newPhotos = window.pictureUsers.slice().sort(function () {
           return 0.5 - Math.random();
         }).slice(0, MAX_PHOTOS);
-        window.renderImg.addPicture(newPhotos);
+        window.render.addPicture(newPhotos);
         break;
       case (evtChange.target.id === 'filter-discussed'):
         clearImg();
         var discussedPhoto = window.pictureUsers.slice().sort(function (a, b) {
           return a.comments.length - b.comments.length;
         }).reverse();
-        window.renderImg.addPicture(discussedPhoto);
+        window.render.addPicture(discussedPhoto);
         break;
     }
   };
